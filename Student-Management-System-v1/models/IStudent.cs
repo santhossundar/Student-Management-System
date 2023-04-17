d291300976ee3d56db9efa6c5b2a93516cbed1fa
santhossundar <santhoshsunthar@gmail.com>
2023-04-17 16:54:12 +0530

    Added student model

---
+++ Student-Management-System-v1/models/IStudent.cs
@@ -0,0 +1,13 @@
+﻿namespace Student_Management_System_v1.models
+{
+    public interface IStudent
+    {
+        string firstName { get; }
+        string lastName { get; }
+        string DOB { get; }
+        string addressNo { get; }
+        string addressStreet { get; }
+        string addressCity { get; }
+        string stdNo { get; }
+    }
+}
