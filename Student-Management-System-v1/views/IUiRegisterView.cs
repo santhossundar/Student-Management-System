6e9b966459958671fe66aa613052e1f72d617648
santhossundar <santhoshsunthar@gmail.com>
2023-04-17 13:26:24 +0530

    Added view interface for register ui

---
+++ Student-Management-System-v1/views/IUiRegisterView.cs
@@ -0,0 +1,13 @@
+﻿namespace Student_Management_System_v1.views
+{
+    public interface IUiRegisterView
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string AddressNo { get; set; }
+        string AddressStreet { get; set; }
+        string AddressCity { get; set; }
+        string DOB { get; set; }
+        string StudentNo { get; set; }
+    }
+}
