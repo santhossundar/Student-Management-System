4a376a04e36b11f9a87ba9afb52db28d32acc6fe
santhossundar <santhoshsunthar@gmail.com>
2023-04-17 13:00:20 +0530

    Added a presenter for main ui

---
+++ Student-Management-System-v1/presenters/UiMainPresenter.cs
@@ -0,0 +1,20 @@
+﻿using Student_Management_System_v1.forms;
+using Student_Management_System_v1.views;
+
+namespace Student_Management_System_v1.presenters
+{
+    public class UiMainPresenter
+    {
+        private IUiMainView uiMainView;
+        public UiMainPresenter(IUiMainView uiMainView) 
+        {
+            this.uiMainView = uiMainView;
+        }
+
+        public void Perform()
+        {
+            UiRegister uiRegister = new UiRegister();
+            uiRegister.Show();
+        }
+    }
+}
