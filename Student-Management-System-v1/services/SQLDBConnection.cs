9bbaeb222cfdab52b1f7e49a8193eb6dcec0ea54
santhossundar <santhoshsunthar@gmail.com>
2023-04-17 22:09:34 +0530

    Added MySql DB connection

---
+++ Student-Management-System-v1/services/SQLDBConnection.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Student_Management_System_v1.services
+{
+    public class SQLDBConnection
+    {
+        public MySqlConnection Connect() 
+        {
+            string server = "localhost";
+            string DB = "bookstall";
+            string userName = "root";
+            string password = "password";
+            string connString = "SERVER=" + server +
+                "; DATABASE=" + DB +
+                "; UID=" + userName +
+                "; PASSWORD=" + password + ";";
+
+            MySqlConnection conn = new MySqlConnection(connString);
+            conn.Open();
+
+            return conn;
+        }
+    }
+}
