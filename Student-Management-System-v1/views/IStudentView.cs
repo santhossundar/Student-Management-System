282036a13174d2a9ebc2f5bb07a2b618cc80a660
santhossundar <santhoshsunthar@gmail.com>
2023-04-18 17:22:15 +0530

    Added student view interface

---
+++ Student-Management-System-v1/views/IStudentView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Management_System_v1.views
+{
+    public interface IStudentView
+    {
+        string title { get; set; }
+    }
+}
