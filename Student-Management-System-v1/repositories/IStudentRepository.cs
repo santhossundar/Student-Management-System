1777c917e508d093310d0f221f7aa5f98871f8b9
santhossundar <santhoshsunthar@gmail.com>
2023-04-27 16:01:36 +0530

    Added Student repository interface

---
+++ Student-Management-System-v1/repositories/IStudentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Student_Management_System_v1.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Management_System_v1.repositories
+{
+    public interface IStudentRepository
+    {
+        void Add(List<string> studentModel);
+    }
+}
