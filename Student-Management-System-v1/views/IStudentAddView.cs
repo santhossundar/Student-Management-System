f575f7def028200f3fd803aef6e4877c0e1a2a6c
santhossundar <santhoshsunthar@gmail.com>
2023-04-27 15:59:02 +0530

    Added an interface for StudentAddView

---
+++ Student-Management-System-v1/views/IStudentAddView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Student_Management_System_v1.views
+{
+    public interface IStudentAddView
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string DOB { get; }
+        string AddressNo { get; set; }
+        string AddressStreet { get; set; }
+        string AddressCity { get; set; }
+        string StdNo { get; set; }
+
+        event EventHandler AddBtnClicked;
+    }
+}
